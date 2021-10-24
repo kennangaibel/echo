fa6c39f3f92d5b537ef4ab47d3b93e7a5b989193
kennangaibel <kennangaibel@gmail.com>
2021-10-23 19:26:26 -0700

    Adding in parse json string to text to audi o

--- functions/index.js
+++ functions/index.js
@@ -12,7 +12,9 @@ const textToSpeech = require('@google-cloud/text-to-speech');
 // any time storage is updated this function will run
 exports.analyzeImage = functions.storage.object().onFinalize(async (object) => {
     const vision_client = new vision.ImageAnnotatorClient();
+// Taking the image and feed it into VISION API
 
+    // Get json string from vision API
     let data = {
         "requests": [
             {
@@ -36,20 +38,42 @@ exports.analyzeImage = functions.storage.object().onFinalize(async (object) => {
     } catch (e) {
         throw new functions.https.HttpsError("internal", e.message, e.details);
     }
-    // Taking the image and feed it into VISION API
-
-    // Get json string from vision API
 
     // Parse json string to text-to-audio format
+    
+    // Vision API will send a json string
+    // Iterate through json Object
+
+    // extract object.name
+    // json will be the array of JSON_objects Nithin returns
+    let json; // output from Nithin here;
+
+    // json string becomes an object through parsing
+    let obj = JSON.parse(json);
+    
+    // Array where names of identified objects will be stored
+    var nameArray = [];
+    // Need to figure out name of jsonArray and how to number through the different objects
+    for (var i = 0; i < jsonArray.length; i++) {
+        // unsure if it should be obj.labelAnnotations.description
+        nameArray.push(obj[i].description);
+    }
+
+    // string that'll be sent to the text-to-speech
+    let speechText = "";
+    for (let i = 0; i < nameArray.length; i++) {
+        speechText = "There is a " + nameArray[i] + " ";
+    }
+
+    // create string result
+
+    // Result needs to be some thing like "this is a [1,2,3,4...]"
 
     // text to speech outputs mp3
-
     // The text to synthesize
     const client = new textToSpeech.TextToSpeechClient();
-
-    const text = "temp"//parsed json string;
     const request = {
-        input: {text: text},
+        input: {text: speechText},
         voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
         audioConfig: {audioEncoding: 'MP3'},
     };
@@ -63,5 +87,9 @@ exports.analyzeImage = functions.storage.object().onFinalize(async (object) => {
         console.log('Audio content written to file: output.mp3');
     }
     parseAudio();
+    // Need to get the mp3 file thats parsed into the firebase storage
+
+
+    
     // flutter reads mp3
 });
